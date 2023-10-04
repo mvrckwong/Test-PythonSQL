8eeecd661c5a01ae57cde94a0157c5cbb90f19dd
Maverick Wong <mvrck.wong@gmail.com>
2023-10-04 19:26:59 +0800

    Update aggregate.sql

--- aggregate.sql
+++ aggregate.sql
@@ -14,22 +14,7 @@ FROM "Spotify"
 WHERE released_year = 2010;
 
 
--- Round the average number of facebook_likes to one decimal place
-SELECT ROUND(AVG(facebook_likes), 1) AS avg_facebook_likes
-FROM reviews;
-
-
--- Calculate the average budget rounded to the thousands
-SELECT ROUND(AVG(budget), -3) AS avg_budget_thousands
-FROM films;
-
-
--- Calculate the title and duration_hours from films
-SELECT title, duration / 60.0 AS duration_hours
-FROM films;
-
-
--- Select the release year, duration, and title sorted by release year and duration
-SELECT release_year, duration, title
-FROM films
-ORDER BY release_year, duration;
+-- Calculate the average streams from the year 2010.
+SELECT ROUND(AVG(streams), 2) as avg_streams_2010
+FROM "Spotify"
+WHERE released_year = 2010;
